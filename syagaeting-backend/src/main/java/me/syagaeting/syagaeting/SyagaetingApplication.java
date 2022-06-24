392a0b6595a245c6f6ed19d8ca035b7de12b5596
JINHWAN KIM <harveydev24@gmail.com>
2022-06-24 15:37:11 +0900

    Initial commit

---
+++ syagaeting-backend/src/main/java/me/syagaeting/syagaeting/SyagaetingApplication.java
@@ -0,0 +1,13 @@
+package me.syagaeting.syagaeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyagaetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyagaetingApplication.class, args);
+	}
+
+}
---
+++ syagaeting-backend/src/test/java/me/syagaeting/syagaeting/SyagaetingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.syagaeting.syagaeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyagaetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
